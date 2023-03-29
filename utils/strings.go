2c52e1cda4dd3bf0338ba6445d72c075650cfce9
Itxaka <itxaka.garcia@spectrocloud.com>
2023-03-29 13:43:01 +0200

    :bug: Recover string utils
    Signed-off-by: Itxaka <itxaka.garcia@spectrocloud.com>

---
+++ utils/strings.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"encoding/json"
+	"gopkg.in/yaml.v1"
+	"math/rand"
+	"strings"
+)
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func ListOutput(rels []string, output string) []string {
+	switch strings.ToLower(output) {
+	case "yaml":
+		d, _ := yaml.Marshal(rels)
+		return []string{string(d)}
+	case "json":
+		d, _ := json.Marshal(rels)
+		return []string{string(d)}
+	default:
+		return rels
+	}
+}
