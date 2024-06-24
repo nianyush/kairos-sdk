51ff23e15a98138b1ac1c209b351c060582f35a3
Piyush Kumar <k17piyush@gmail.com>
2024-06-25 01:06:31 +0530

    added custom root and config flag in cluster properties

--- clusterplugin/plugin.go
+++ clusterplugin/plugin.go
@@ -20,8 +20,7 @@ type ClusterProvider func(cluster Cluster) yip.YipConfig
 // ClusterPlugin creates a cluster plugin from a `ClusterProvider`.  It calls the cluster provider at the appropriate events
 // and ensures it configuration is written where it will be executed.
 type ClusterPlugin struct {
-	Provider          ClusterProvider
-	ClusterConfigPath string
+	Provider ClusterProvider
 }
 
 func (p ClusterPlugin) onBoot(event *pluggable.Event) pluggable.EventResponse {
@@ -48,11 +47,14 @@ func (p ClusterPlugin) onBoot(event *pluggable.Event) pluggable.EventResponse {
 	// request the cloud configuration of the provider
 	cc := p.Provider(*config.Cluster)
 
-	// open our cloud configuration file for writing
-	if len(p.ClusterConfigPath) == 0 {
-		p.ClusterConfigPath = clusterProviderCloudConfigFile
+	configFilePath := clusterProviderCloudConfigFile
+
+	if len(config.Cluster.ClusterConfigPath) != 0 {
+		configFilePath = config.Cluster.ClusterConfigPath
 	}
-	f, err := filesystem.OpenFile(p.ClusterConfigPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+
+	// open our cloud configuration file for writing
+	f, err := filesystem.OpenFile(configFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
 	if err != nil {
 		response.Error = fmt.Sprintf("failed to parse boot event: %s", err.Error())
 		return response
